97eab2f73ae5394b42304d7c6d579033e2a82170
தினேஷ் கருணாகரன் | Dinesh Karunakaran <68123217+DineshK1910@users.noreply.github.com>
2023-06-09 21:54:38 +0530

    Update tumor_cnn.py

--- tumor_cnn.py
+++ tumor_cnn.py
@@ -8,17 +8,11 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense,Dropout
 from tensorflow.keras.utils import to_categorical
 
-
 target_size = (150, 150)
-
 trdataset_path = "/kaggle/input/brain-tumor-classification-mri/Training"
 tsdataset_path = "/kaggle/input/brain-tumor-classification-mri/Testing"
-
-
 classes = ["glioma_tumor",'meningioma_tumor', "no_tumor",'pituitary_tumor']
-
-
-def preprocess_image(image_path, target_size):
+def transformm(image_path, target_size):
     image = cv2.imread(image_path)
     image = cv2.resize(image, target_size)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -32,24 +26,20 @@ for class_idx, class_name in enumerate(classes):
     class_folder = os.path.join(trdataset_path, class_name)
     for filename in os.listdir(class_folder):
         image_path = os.path.join(class_folder, filename)
-        preprocessed_image = preprocess_image(image_path, target_size)
-        X.append(preprocessed_image)
+        transformm = transformm(image_path, target_size)
+        X.append(transformm)
         y.append(class_idx)
 for class_idx, class_name in enumerate(classes):
     class_folder = os.path.join(tsdataset_path, class_name)
     for filename in os.listdir(class_folder):
         image_path = os.path.join(class_folder, filename)
-        preprocessed_image = preprocess_image(image_path, target_size)
-        X.append(preprocessed_image)
+        transformm = transformm(image_path, target_size)
+        X.append(transformm)
         y.append(class_idx)
 
-X = np.array(X)
+        X = np.array(X)
 y = np.array(y)
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
 num_classes = len(classes)
 y_train = to_categorical(y_train, num_classes)
 y_test = to_categorical(y_test, num_classes)
@@ -70,9 +60,7 @@ model.add(Dense(128, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation="softmax"))
 
-
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-
 
 model.fit(X_train, y_train, batch_size=32, epochs=15, validation_data=(X_test, y_test))
 
