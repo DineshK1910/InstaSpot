90191242d24390855f6e6d8c12524a2917d7ee79
தினேஷ் கருணாகரன் | Dinesh Karunakaran <68123217+DineshK1910@users.noreply.github.com>
2023-06-16 14:26:21 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
 import pickle
+import tensorflow as tf
 with open('Model.pkl', 'rb') as file:
     model = pickle.load(file)
+new_model = tf.keras.models.load_model('my_model.h5')
 target_size = (150, 150)
 
 
@@ -12,7 +14,7 @@ def preprocess_image(image_path, target_size):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = image.astype(np.float32) / 255.0
     input_image = np.expand_dims(image, axis=0)
-    predictions = model.predict(input_image)
+    predictions = new_model.predict(input_image)
     predicted_class = np.argmax(predictions)
     tumor_type = ["glioma_tumor", 'meningioma_tumor',
                   "no_tumor", 'pituitary_tumor']
